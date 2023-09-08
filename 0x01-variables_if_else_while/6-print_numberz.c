a3251ea3e0ccd0a6ca24b0b1ed9d3ed87b70bffa
Abdalla-Mohsen10 <136993087+Abdalla-Mohsen10@users.noreply.github.com>
2023-09-08 16:17:21 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,16 +5,15 @@
  * Return: Always 0 (success/correct)
  */
 int main(void)
-
 {
 int i;
 
 for (i = 0 ; i < 10; i++)
 {
-printf("%i", i);
+putchar(i);
 }
 
-printf("\n");
+putchar("\n");
 
 return (0);
 
