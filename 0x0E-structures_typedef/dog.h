136f9990ece22f22b68f2100fadf936cbd523f87
Abdalla-Mohsen10 <136993087+Abdalla-Mohsen10@users.noreply.github.com>
2023-10-11 19:29:11 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
-#include <stdio.h>
-#include "0-object_like_macro.h"
-#include "1-pi.h"
-#include "3-function_like_macro.h"
-#include "4-sum.h"
+/**
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner:  owner
+ * Description: This struct is for dog
+ */
 
-#endif 
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
